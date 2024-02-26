cf8dc059374de19c7496c9630fd75ffb0093cd24
Rafael <rjribeiro@inf.ufrgs.br>
2024-02-26 17:20:29 -0300

    Configure main.go

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
+compose_build:
+	docker compose --project-name gofipe -f ./deployments/docker-compose.yaml up --build
 build-mocks:
 	${GOROOT}/bin/go generate ./...
--- cmd/goFipe/main.go
+++ cmd/goFipe/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"github.com/raffops/gofipe/cmd/goFipe/controller/rest"
+	"github.com/raffops/gofipe/cmd/goFipe/database/postgres"
+	"github.com/raffops/gofipe/cmd/goFipe/logger"
+	postgresRepo "github.com/raffops/gofipe/cmd/goFipe/repository/postgres"
+	"github.com/raffops/gofipe/cmd/goFipe/service"
+	"github.com/raffops/gofipe/cmd/goFipe/utils"
+)
 
+func main() {
+	logger.Info("Starting api...")
+	errEnvVariables := utils.LoadEnvVariables()
+	if errEnvVariables != nil {
+		logger.Fatal("Error loading environment variables", logger.String("error", errEnvVariables.Message))
+	}
+	postgresConn := postgres.GetPostgresConnection()
+	defer postgres.ClosePostgresConnection(postgresConn)
+
+	vehicleRepo := postgresRepo.NewVehicleRepositoryPostgres(postgresConn)
+	vehicleService := service.NewVehicleService(vehicleRepo)
+	rest.Start(vehicleService)
 }
