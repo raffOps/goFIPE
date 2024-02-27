6617450a345dc9de6737c70e39d82e44bed79786
Rafael <rjribeiro@inf.ufrgs.br>
2024-02-27 18:05:18 -0300

    Remove commented code

--- cmd/goFipe/main.go
+++ cmd/goFipe/main.go
@@ -3,18 +3,11 @@ package main
 import (
 	"github.com/raffops/gofipe/cmd/goFipe/controller/rest"
 	"github.com/raffops/gofipe/cmd/goFipe/database/postgres"
-	"github.com/raffops/gofipe/cmd/goFipe/logger"
 	postgresRepo "github.com/raffops/gofipe/cmd/goFipe/repository/postgres"
 	"github.com/raffops/gofipe/cmd/goFipe/service"
 )
 
 func main() {
-	logger.Info("Starting api...")
-	//errEnvVariables := utils.LoadEnvVariables()
-	//if errEnvVariables != nil {
-	//	logger.Fatal("Error loading environment variables", logger.String("error", errEnvVariables.Message))
-	//}
-
 	postgresConn := postgres.GetPostgresConnection()
 	defer postgres.ClosePostgresConnection(postgresConn)
 
